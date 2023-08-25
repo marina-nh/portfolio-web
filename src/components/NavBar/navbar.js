c8758fb5f3c285a47deb280ac1316c7afef50f24
marina-nh <herediamarinarocio@gmail.com>
2023-08-25 13:52:34 -0300

    Navbar modification

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='navbar'>
+        <img src='' alt=''/>
+        <div className='desktopMenu'>
+
+        </div>
+        <button className='desktopMenuBtn'>
+            
+        </button>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
