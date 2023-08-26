f413015c281e1255eedceec37dbb157bbe0ce6b8
marina-nh <herediamarinarocio@gmail.com>
2023-08-25 21:32:23 -0300

    navbar.js modification logo

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
-        <img src='' alt=''/>
+        <img src={logo} alt='logo' className='logo'/>
         <div className='desktopMenu'>
 
         </div>
         <button className='desktopMenuBtn'>
-            
-        </button>
+            <img src='' alt='' className='desktopMenuImg'/>Contacto</button>
     </nav>
   )
 }
