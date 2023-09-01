d7b76ea63974806be18fe971cd981623cc8ad64a
marina-nh <herediamarinarocio@gmail.com>
2023-08-31 22:19:17 -0300

    Skills modifications

---
+++ src/components/Skills/skills.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/webdesign.png'; 
+import AppDesign from '../../assets/app-design.png';
+
+const Skills = () => {
+  return (
+    <section id='skills'>
+        <span className='SkillTitle'>What I do</span>
+        <span className='SkillDesc'>I am a skilled and passionate web designer with experience in creating visually appealing and user-friendy</span>
+        <div className='skillBars'>
+            <div className='skillBar'>
+                <img src={UIDesign} alt='UIDesign' className='skillBarImg'/>
+                <div className='skillBarText'>
+                    <h2>UI/UX Design</h2>
+                    <p>This is a demo text, you can write your own content</p>
+                </div>
+            </div>
+            <div className='skillBar'>
+                <img src={WebDesign} alt='WebDesign' className='skillBarImg'/>
+                <div className='skillBarText'>
+                    <h2></h2>
+                    <p>This is a demo text, you can write your own content</p>
+                </div>
+            </div>
+            <div className='skillBar'>
+                <img src={AppDesign} alt='AppDesign' className='skillBarImg'/>
+                <div className='skillBarText'>
+                    <h2></h2>
+                    <p>This is a demo text, you can write your own content</p>
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default Skills
