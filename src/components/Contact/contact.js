b6b2586f11befad723ff0d41962957a246f06fa1
marina-nh <herediamarinarocio@gmail.com>
2023-09-04 21:34:50 -0300

    Email service has been added.

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import client1 from '../../assets/client-1.png';
 import client2 from '../../assets/client-2.png';
@@ -8,42 +8,54 @@ import LinkedInIcon from '../../assets/linkedin-icon.png';
 import GitHubIcon from '../../assets/github-icon.png';
 import TwitterIcon from '../../assets/twitter-icon.png';
 import YouTubeIcon from '../../assets/youtube-icon.png';
+import emailjs from '@emailjs/browser';
 
-const  Contact = () => {
-  return (
-    <section id='contactPage'>
-        <div id='clients'>
-            <h1 className='contactPageTitle'>My Clients</h1>
-            <p className=''>
-                I have had the opportunity to work with a diverse group of companies.
-                Some of the notable companies I have worked with includes
-            </p>
-            <div className='clientImgs'>
-                <img src={client1} alt='' className='clientImg' />
-                <img src={client2} alt='' className='clientImg' />
-                <img src={client3} alt='' className='clientImg' />
-                <img src={client4} alt='' className='clientImg' />
-            </div>
-        </div>
-        <div id='contact'>
-            <h1 className='contactPageTitle'>Contact Me</h1>
-            <span className='contactDesc'> Please fill out the form below to discuss any work opportunities</span>
-            <form className='contactForm'>
-                <input type='text' className='name' placeholder='Your Name'/>
-                <input type='email' className='email' placeholder='Your Email'/>
-                <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
-                <button type='submit' value="Send" className='submitBtn'>Submit</button>
-                <div className='links'>
-                    <img src={LinkedInIcon} alt='LinkedIn' className='link'/>
-                    <img src={GitHubIcon} alt='GitHub' className='link'/>
-                    <img src={TwitterIcon} alt='Twitter' className='link' />
-                    <img src={YouTubeIcon} alt='YouTube' className='link' />
+const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+    return (
+        <section id='contactPage'>
+            <div id='clients'>
+                <h1 className='contactPageTitle'>My Clients</h1>
+                <p className=''>
+                    I have had the opportunity to work with a diverse group of companies.
+                    Some of the notable companies I have worked with includes
+                </p>
+                <div className='clientImgs'>
+                    <img src={client1} alt='' className='clientImg' />
+                    <img src={client2} alt='' className='clientImg' />
+                    <img src={client3} alt='' className='clientImg' />
+                    <img src={client4} alt='' className='clientImg' />
                 </div>
-            </form>
-        </div>
-    </section>
+            </div>
+            <div id='contact'>
+                <h1 className='contactPageTitle'>Contact Me</h1>
+                <span className='contactDesc'> Please fill out the form below to discuss any work opportunities</span>
+                <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type='text' className='name' placeholder='Your Name' />
+                    <input type='email' className='email' placeholder='Your Email' />
+                    <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
+                    <button type='submit' value="Send" className='submitBtn'>Submit</button>
+                    <div className='links'>
+                        <img src={LinkedInIcon} alt='LinkedIn' className='link' />
+                        <img src={GitHubIcon} alt='GitHub' className='link' />
+                        <img src={TwitterIcon} alt='Twitter' className='link' />
+                        <img src={YouTubeIcon} alt='YouTube' className='link' />
+                    </div>
+                </form>
+            </div>
+        </section>
 
-  );
+    );
 }
 
 export default Contact
