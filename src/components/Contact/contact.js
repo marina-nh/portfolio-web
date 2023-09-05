4b366827831d37d1febeeb1ec501c1ad285433a0
marina-nh <herediamarinarocio@gmail.com>
2023-09-05 00:41:21 -0300

    Finished

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -16,8 +16,11 @@ const Contact = () => {
         e.preventDefault();
 
         emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+            //Se debe agregar la id cuando se registre en la pagina emailjs 
             .then((result) => {
                 console.log(result.text);
+                e.target.reset();
+                alert('Email Sent!');
             }, (error) => {
                 console.log(error.text);
             });
@@ -41,8 +44,8 @@ const Contact = () => {
                 <h1 className='contactPageTitle'>Contact Me</h1>
                 <span className='contactDesc'> Please fill out the form below to discuss any work opportunities</span>
                 <form className='contactForm' ref={form} onSubmit={sendEmail}>
-                    <input type='text' className='name' placeholder='Your Name' />
-                    <input type='email' className='email' placeholder='Your Email' />
+                    <input type='text' className='name' placeholder='Your Name' name='your_name'/>
+                    <input type='email' className='email' placeholder='Your Email' name='your_email'/>
                     <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
                     <button type='submit' value="Send" className='submitBtn'>Submit</button>
                     <div className='links'>
